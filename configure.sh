7b39113123121ceb5b66cdeee793459a6dfec36d
Jason Ernst <jason@left.io>
2018-02-07 14:18:20 -0800

    initial packages

--- configure.sh
+++ configure.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+# installs the necessary packages on ubuntu
+sudo apt-get update
+sudo apt-get install build-essential libssl-dev
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | sh
 
