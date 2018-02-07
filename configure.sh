9e34d29d7d47cf6ed6d7276ca7863630b6978197
Jason Ernst <jason@left.io>
2018-02-07 14:33:49 -0800

    added more deps for ethereum, curl

--- configure.sh
+++ configure.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # installs the necessary packages on ubuntu
 sudo apt-get update
-sudo apt-get install build-essential libssl-dev
+sudo apt-get install build-essential libssl-dev curl software-properties-common
+sudo add-apt-repository -y ppa:ethereum/ethereum
+sudo apt-get update
+sudo apt-get install ethereum
+
+# npm, node
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | sh
 
